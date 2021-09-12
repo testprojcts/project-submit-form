9e84a2542ee9652641becbd2912b9404b9ad3ea3
Test Projects 83 <testprojects83@gmail.com>
2021-09-12 12:43:44 +0100

    updated the source codes

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAXzo_PlDRN1fhqlBUnXFx4gJrDUHizjw',
+  authDomain: 'project-submit-auth.firebaseapp.com',
+  projectId: 'project-submit-auth',
+  storageBucket: 'project-submit-auth.appspot.com',
+  messagingSenderId: '463211525415',
+  appId: '1:463211525415:web:9fd720285cc6661527a502',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
